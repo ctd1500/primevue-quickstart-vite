25dc7a111faca0c7cb6f58091012709bf88b64bd
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-05-03 19:30:34 +0200

    chore: use vite-ssg with cjs format

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import routes from '~pages'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';          
+export const createApp = ViteSSG(
+  App,
+  { routes, base: import.meta.env.BASE_URL },
+  ({  app }) => {
+    // install all modules under `modules/`
+    app.use(PrimeVue)
+    app.component('Button', Button)
+  },
+)
 
-const app = createApp(App);
-app.use(PrimeVue);
-app.component('Button', Button);
-
-app.mount('#app');
