49e1bcb9fdca0984602f175c916a499af637559f
ctd1500 <ctd1500@gmail.com>
2024-01-28 20:09:10 -0800

    Fixed builds

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,32 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Pages from "vite-plugin-pages";
+import resolve from "@rollup/plugin-node-resolve";
 
-// https://vitejs.dev/config/
+const customResolver = resolve({
+	extensions: [".esm.js", ".css", ".scss"],
+});
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    Pages({
-      extensions: ['vue', 'md'],
-    })
-  ],
-  ssgOptions: {
-    script: 'async',
-    format: 'cjs',
-  },
-})
+	plugins: [
+		vue(),
+		Pages({
+			extensions: ["vue", "md"],
+		}),
+	],
+	ssgOptions: {
+		script: "async",
+		formatting: "prettify",
+	},
+	resolve: {
+		alias: [
+			{
+				find: "primevue",
+				customResolver: customResolver,
+				replacement: function (alias) {
+					return alias;
+				},
+			},
+		],
+	},
+});
